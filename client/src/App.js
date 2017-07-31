1fbc9a90a3028a899baa28c37cd3085690e56bd9
Pavel Naydenov <naydenov.p.v@gmail.com>
2017-08-01 01:04:33 +0300

    material-ui, flow, react-flexbox deps

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
+// @flow
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton'
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
@@ -13,6 +21,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <MuiThemeProvider>
+          <Grid fluid>
+            <Row>
+              <Col xs={12} md={4}>
+                <RaisedButton label="hello 1" />
+              </Col>
+              <Col xs={6} md={4}>
+                <RaisedButton label="hello 2" />
+              </Col>
+              <Col xs={6} md={4}>
+                <RaisedButton label="hello 3" />
+              </Col>
+            </Row>
+          </Grid>
+        </MuiThemeProvider>
       </div>
     );
   }
