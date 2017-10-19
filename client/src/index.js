03f25150f70fca273bdba81c4c5056cd3c259256
Pavel Naydenov <naydenov.p.v@gmail.com>
2017-10-19 09:45:24 +0300

    add underscore

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+
+import Menu from './components/Menu'
+import data from './components/data'
+ReactDOM.render(<Menu recipes={data} />, document.getElementById("react-container"))
